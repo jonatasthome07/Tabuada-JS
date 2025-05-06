084ed97177dae63caec808eea37b96b3ea5b675b
Jônatas <jonatasthome04@gmail.com>
2025-05-06 01:13:17 -0300

    Updating JS - Function, loop and template string

--- js/script.js
+++ js/script.js
@@ -1,6 +1,28 @@
 const multiplicationForm = document.querySelector("#multiplication-form")
 const numberInput = document.querySelector("#number")
 const multiplicationInput = document.querySelector("#multiplicator")
+const multiplicationTable = document.querySelector("#multiplication-operations")
+const multiplicationTitle = document.querySelector("#multiplication-title span")
+
+const createTable = (number, multiplicatorNumber) => {
+    multiplicationTable.innerHTML = "";
+    
+    for (i = 1; i <=multiplicatorNumber; i++){
+        const result = number * i;
+        
+        const template = `<div class="row">
+        <div class="operation">${number} x ${i} = </div>
+        <div class="result"> ${result}</div>
+        </div>`;
+
+        const parser = new DOMParser()
+        const htmlTemplate = parser.parseFromString(template, "text/html")
+        const row = htmlTemplate.querySelector(".row")
+        multiplicationTable.appendChild(row);
+
+        multiplicationTitle.innerText = number;
+    }
+}
 
 multiplicationForm.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -11,5 +33,5 @@ multiplicationForm.addEventListener("submit", (e) => {
         alert("Ambos os campos devem ser preenchidos")
     }
 
-    console.log(multiplicationNumber, multiplicatorNumber)
+    createTable(multiplicationNumber, multiplicatorNumber)
 })
